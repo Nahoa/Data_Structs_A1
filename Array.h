20f873a292feac14c31ca3b39a6042c863380ff1
Nahoa <rraymond001@gmail.com>
2017-01-10 12:12:47 -0800

    Update Array.h
    1/10/17
    updated and completed the Array.h

--- Array.h
+++ Array.h
@@ -3,19 +3,137 @@
 template <typename T> class Array
 {
 public:
-	Array();
-	Array(int length, int start_index = 0); // sets the start index to zero
-	Array(const Array & copy); //Copy Ctor 
-	~Array();
-	Array& operator=(const Array &rhs); // overloading the = assignment operator
-	T & operator[](int index); //overloads the template [] operator 
-	int GetStartIndex(); //returns the starting index of the array
-	void SetStartIndex(int start_index); // sets the starting index of the array
-	int GetLength(); //gets the Length of the array
-	void SetLength(int Length);// sets the length of the array
+	Array()
+	{
+		m_length = 0; // sets 0 as the default length 
+		m_start_index = 0; // sets starting index to 0
+		m_array = new T[m_length]; // creates a templated array with 0 entries in the array
+	};
+
+	Array(int length, int start_index = 0)// sets the start index to zero
+	{
+		m_length = length;
+		m_start_index = start_index;
+
+		m_array = new T[m_length];
+	}; 
+
+
+	Array(const Array & copy):m_array(nullptr),m_length(0),m_start_index(0)//Copy Ctor
+	{
+		if (copy.m_array != nullptr)
+		{
+			m_length = copy.m_length;
+			m_start_index = copy.m_start_index;
+
+			m_array = new T[m_length];
+
+			for (int i = m_start_index; i < m_length; i++)//deep copy
+			{
+				 m_array[i] = copy.m_array[i]; // copies anything that is in the OG array into the copyied array
+			}
+		}
+	};  
+
+	~Array()
+	{
+		delete[] m_array;
+		m_array = nullptr;
+		m_length = 0; // restores the length to a default state
+		m_start_index = 0; //restores the length to a default state
+	};
+
+	Array& operator=(const Array &rhs) // overloading the = assignment operator
+	{
+		if (this != &rhs) //checks to see if the left side element is not the same as the right side element
+		{
+			if (rhs.m_array != nullptr)
+			{
+				delete[] m_array;
+				m_length = rhs.m_length;
+				m_start_index = rhs.m_start_index;
+
+				m_array = new T[m_length];
+
+				for (int i = 0; i < m_length; i++) // deep copy
+				{
+					m_array[i] = rhs.m_array[i];
+				}
+			}
+			
+		}
+		return *this;
+	};
+	T & operator[](int index) //overloads the template [] operator 
+	{
+		index = index - m_start_index;
+
+		if (index >= m_length) //checks to see if the thing being checked is in the length range
+		{
+			//throws exception that the index is above the range
+			throw Exception("Above Range");
+		}
+		else if (index < 0) //checks to see if the index being accessed is above the start index
+		{
+			//throws exception that the index is below the range
+			throw Exception("Below Range");
+		}
+		else
+		{
+			return m_array[index];
+		}
+	};
+	int GetStartIndex() //returns the starting index of the array
+	{
+		return m_start_index;
+	}
+	void SetStartIndex(int start_index) // sets the starting index of the array
+	{
+		m_start_index = start_index;
+
+	};
+	int GetLength() //gets the Length of the array
+	{
+		return m_length;
+
+	};
+	void SetLength(int Length) // sets the length of the array
+	{
+		if (Length < 0)
+		{
+			throw Exception("You cannot have a negative array!");
+		}
+		else 
+		{
+			int len = Length < m_length ? Length : m_length; //checks to see if the array is bigger or smaller and how much space is needed to copy
+
+			T * temp = new T[Length]; // creates a new temp array
+
+			for (int i = 0; i < len; i++) // copies all the elements from the old array into the new temp array
+			{
+				temp[i] = m_array[i];
+			}
+
+			delete[] m_array; // deletes the old array
+			m_array = nullptr;
+
+			m_array = new T[Length]; //makes a new array with the new length
+
+			for (int i = 0; i < Length; i++) // puts everything in the temp array into the new array
+			{
+				m_array[i] = temp[i];
+			}
+
+			m_length = Length;
+
+			delete[]temp; //deletes the old array
+			temp = nullptr;
+
+		}
+	};
 
 private:
-	T * m_array; 
+	T * m_array;
 	int m_length;
 	int m_start_index;
 };
